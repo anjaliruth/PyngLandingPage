d15dfa50e6a116350e95bdbbc1db06dd1a940cfe
anjaliruth <ruthanjali0211@gmail.com>
2023-07-13 22:10:36 +0800

    tried to change font

---
+++ FirstProject/App.js
@@ -0,0 +1,61 @@
+import { StatusBar } from "expo-status-bar";
+import React from 'react';
+
+import { StyleSheet, Text, View } from "react-native";
+import logo from "./Pyng.png";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.image}>
+        <img src={logo} alt="logo" />
+      </View>
+      <View style={styles.buttons}>
+        <button style={styles.button1}>Login</button>
+        <button style={styles.button2}>Sign Up</button>
+      </View>
+
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+
+  container: {
+    display: "flex",
+
+    backgroundColor: "#EFEEEE",
+    alignItems: "center",
+    justifyContent: "space-between",
+    width: "100%",
+  },
+  image: {
+    marginTop: "200px",
+    width: "60%",
+  },
+
+  button1: {
+    fontSize: "1.3rem",
+    borderRadius: "20px",
+    backgroundColor: "#FE07C8",
+    color:"#EFEEEE",
+    width:"300px",
+    border: " 1px solid #000000",
+    padding: "5px",
+marginBottom: "5px",
+// fontFamily: "Comfortaa"
+
+  },
+  button2: {
+    fontSize: "1.3rem",
+    borderRadius: "20px",
+    backgroundColor: "#AA47AC",
+    color:"#EFEEEE",
+    width:"300px",
+    border: " 1px solid #A010A3",
+    padding: "5px",
+    marginBottom: "5px", 
+    // fontFamily: "Comfortaa"
+  },
+});
